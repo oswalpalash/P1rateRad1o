95e3a8181e7e27d4c412c64708f40de655b25d32
Palash <oswalpalash@gmail.com>
2020-01-04 11:55:54 +0530

    Set up initial tree

---
+++ P1rateRad1o/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for P1rateRad1o project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'P1rateRad1o.settings')
+
+application = get_asgi_application()
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # P1rateRad1o
 Django App to Interface with a Radio Device to send POCSAG messages
+
+## Steps to Run the Project
+	1. pip3 install django
+	2. export SECRET_KEY=<mysecretkey>
+	3. python3 manage.py runserver
+
+Data is stored in a local sqlite3 DB
