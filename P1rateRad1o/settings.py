0f60abf13bd27f993fd82b01779bdfd61cd38d39
Palash <oswalpalash@gmail.com>
2020-01-04 12:03:23 +0530

    Add Pager app

--- P1rateRad1o/settings.py
+++ P1rateRad1o/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pager'
 ]
 
 MIDDLEWARE = [
